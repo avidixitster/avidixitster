d921f5e0c9dbed328d0b40d7152b7b9d8c906564
avi dixit <adixit@icoachfirst1.onmicrosoft.com>
2021-08-04 00:26:24 +0530

    first commit

---
+++ Database1/ExtendedProperties.sql
@@ -0,0 +1,6 @@
+﻿EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'AdventureWorks 2014 Sample OLTP Database';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Primary filegroup for the AdventureWorks 2014 sample database.', @level0type = N'FILEGROUP', @level0name = N'PRIMARY';
+
